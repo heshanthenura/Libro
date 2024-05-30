d397443c4b40fe7b0f20db43be9476ea2159a06a
Heshan Thenura <heshanthenura@protonmail.com>
2024-05-30 23:16:06 +0530

    added logout

--- src/main/java/com/heshanthenura/libro/controllers/MainController.java
+++ src/main/java/com/heshanthenura/libro/controllers/MainController.java
@@ -1,11 +1,42 @@
 package com.heshanthenura.libro.controllers;
 
+import com.heshanthenura.libro.MainApplication;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainController implements Initializable {
+
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private void logoutFunc() {
+        // Get the current stage and close it
+        Stage currentStage = (Stage) logoutBtn.getScene().getWindow();
+        currentStage.close();
+
+        // Load and display the login.fxml file
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("login.fxml"));
+            Parent root = fxmlLoader.load();
+            Stage loginStage = new Stage();
+            loginStage.setScene(new Scene(root));
+            loginStage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
 
